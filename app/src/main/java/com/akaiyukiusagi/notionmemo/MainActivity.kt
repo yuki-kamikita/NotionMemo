dc59d51f14cf469cb7ed9cd9f835dda9e15ba6f0
shuhei_kiyosawa <s.kiyosawa@frontierfield.co.jp>
2020-09-25 21:32:50 +0900

    start

---
+++ app/src/main/java/com/akaiyukiusagi/notionmemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.akaiyukiusagi.notionmemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
